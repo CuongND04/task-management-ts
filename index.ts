72673858d3d60dfea383cdb9aeb7944e3290906e
Nguyễn Đức Cường <uteu237@gmail.com>
2024-07-26 00:32:38 +0700

    ♻️ Tạo routes

--- index.ts
+++ index.ts
@@ -1,30 +1,17 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotnev from "dotenv";
 import * as database from "./config/database";
-import Task from "./models/task.model";
+import mainV1Routes from "./api/v1/routes/index.route";
+
 dotnev.config();
+
 database.connect();
+
 const app: Express = express();
 const port: string | number = process.env.PORT || 3000;
-app.get("/tasks", async (req: Request, res: Response) => {
-  const tasks = await Task.find({
-    deleted: false,
-  });
-  res.json(tasks);
-});
-app.get(
-  "/tasks/detail/:id",
-  async (req: Request, res: Response): Promise<void> => {
-    const id: string = req.params.id;
 
-    const task = await Task.findOne({
-      _id: id,
-      deleted: false,
-    });
+mainV1Routes(app);
 
-    res.json(task);
-  }
-);
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
