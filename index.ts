595a383fdb41795113c9149e04e93a54354ab6bf
Nguyễn Đức Cường <uteu237@gmail.com>
2024-07-26 00:00:22 +0700

    ✨ 7.2. Lấy ra danh sách công việc

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("OK 123");
+import express, { Express, Request, Response } from "express";
+import dotnev from "dotenv";
+import * as database from "./config/database";
+import Task from "./models/task.model";
+dotnev.config();
+database.connect();
+const app: Express = express();
+const port: string | number = process.env.PORT || 3000;
+app.get("/tasks", async (req: Request, res: Response) => {
+  const tasks = await Task.find({
+    deleted: false,
+  });
+  res.json(tasks);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
