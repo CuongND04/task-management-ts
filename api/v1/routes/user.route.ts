e21a83aea8dab12dba456ff651f503964d0a1de1
Nguyễn Đức Cường <uteu237@gmail.com>
2024-07-26 11:05:32 +0700

    ✨ Tính năng đăng nhập

--- api/v1/routes/user.route.ts
+++ api/v1/routes/user.route.ts
@@ -3,5 +3,6 @@ import * as controller from "../controllers/user.controller";
 const router: Router = express.Router();
 
 router.post("/register", controller.register);
+router.post("/login", controller.login);
 
 export const userRoutes: Router = router;
